3998df6751b9150c935e3db231d72a22275a1e57
김용건 <yonggeon.kim@gmail.com>
2024-07-23 17:50:15 +0900

    feat: authorization server supporting client_credentials

---
+++ sping-boot-3-cookbook/footballauth/src/test/java/org/demo/footballauth/FootballauthApplicationTests.java
@@ -0,0 +1,13 @@
+package org.demo.footballauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballauthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
