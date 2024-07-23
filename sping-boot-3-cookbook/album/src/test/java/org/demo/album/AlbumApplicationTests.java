63828098b2284305148ed34a66a32a0bee102527
김용건 <yonggeon.kim@gmail.com>
2024-07-23 12:10:32 +0900

    feat: initial album project

---
+++ sping-boot-3-cookbook/album/src/test/java/org/demo/album/AlbumApplicationTests.java
@@ -0,0 +1,13 @@
+package org.demo.album;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlbumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
