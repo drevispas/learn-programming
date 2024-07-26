f651bbfe107103816731d409e45ce8e24d4ce59b
김용건 <yonggeon.kim@gmail.com>
2024-07-26 15:41:23 +0900

    feat: add actuate endpoints

--- sping-boot-3-cookbook/footballresource/src/main/java/org/demo/footballresource/security/SecurityConfig.java
+++ sping-boot-3-cookbook/footballresource/src/main/java/org/demo/footballresource/security/SecurityConfig.java
@@ -17,12 +17,14 @@ public class SecurityConfig {
                 authorizeRequests
                         // Spring default JwtAuthenticationConverter will be used to convert JWT to Authentication
                         // "SCOPE_" prefix is added to the scope value by JwtAuthenticationConverter
-                        .requestMatchers(HttpMethod.GET, "/football/teams/**")
-                        .hasAuthority("SCOPE_football:read")
+                        .requestMatchers(HttpMethod.GET, "/football/teams/**").hasAuthority("SCOPE_football:read")
                         // POST should have admin scope, or it will be forbidden with 403
-                        .requestMatchers(HttpMethod.POST, "/football/teams/**")
-                        .hasAuthority("SCOPE_football:admin")
-                        .anyRequest().authenticated())
+                        .requestMatchers(HttpMethod.POST, "/football/teams/**").hasAuthority("SCOPE_football:admin")
+                        .requestMatchers("/actuator/health").permitAll()
+                        .requestMatchers("/actuator/**").hasAuthority("SCOPE_football:admin")
+                        .anyRequest().authenticated()
+                )
+                // Configure OAuth2 resource server to accept JWT
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .build();
     }
