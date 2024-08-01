96a161117ead2cab75137f87328cacb41a9898b2
김용건 <yonggeon.kim@gmail.com>
2024-08-01 18:48:10 +0900

    feat: fresh footballregistry service

---
+++ sping-boot-3-cookbook/footballregistry/src/main/java/org/demo/footballregistry/FootballregistryApplication.java
@@ -0,0 +1,13 @@
+package org.demo.footballregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballregistryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FootballregistryApplication.class, args);
+    }
+
+}
---
+++ sping-boot-3-cookbook/footballregistry/src/test/java/org/demo/footballregistry/FootballregistryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.demo.footballregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballregistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
