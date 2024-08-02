f78f522227028c3e20b405bb161b85baf775fef0
김용건 <yonggeon.kim@gmail.com>
2024-08-02 19:54:13 +0900

    feat: initial spring cloud gateway project

---
+++ sping-boot-3-cookbook/footballgateway/src/test/java/org/demo/footballgateway/FootballgatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.demo.footballgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballgatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
