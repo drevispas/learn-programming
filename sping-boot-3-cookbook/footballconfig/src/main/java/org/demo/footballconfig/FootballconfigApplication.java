1db56a9434f0bb690f8cb8326cf3a767b1856897
김용건 <yonggeon.kim@gmail.com>
2024-08-03 11:30:43 +0900

    feat: initial spring cloud config project

---
+++ sping-boot-3-cookbook/footballconfig/src/main/java/org/demo/footballconfig/FootballconfigApplication.java
@@ -0,0 +1,13 @@
+package org.demo.footballconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballconfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FootballconfigApplication.class, args);
+    }
+
+}
