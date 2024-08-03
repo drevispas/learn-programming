b9193c3af75b597d8990419b161075f122914ef1
김용건 <yonggeon.kim@gmail.com>
2024-08-03 14:53:23 +0900

    feat: initial SB Admin project

---
+++ sping-boot-3-cookbook/footballadmin/build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.3.2"
+	id("io.spring.dependency-management") version "1.1.6"
+}
+
+group = "org.demo"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+extra["springBootAdminVersion"] = "3.3.2"
+extra["springCloudVersion"] = "2023.0.3"
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("de.codecentric:spring-boot-admin-starter-server")
+	implementation("org.springframework.cloud:spring-cloud-starter-config")
+	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("de.codecentric:spring-boot-admin-dependencies:${property("springBootAdminVersion")}")
+		mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ sping-boot-3-cookbook/footballadmin/src/main/java/org/demo/footballadmin/FootballadminApplication.java
@@ -0,0 +1,13 @@
+package org.demo.footballadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballadminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballadminApplication.class, args);
+	}
+
+}
