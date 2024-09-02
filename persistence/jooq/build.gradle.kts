a3fb8db7fe3318fb8549d301cb86c99e96044b3d
김용건 <yonggeon.kim@gmail.com>
2024-09-02 20:59:15 +0900

    (jooq) feat: build.gradle.kts for jooq

--- persistence/jooq/build.gradle.kts
+++ persistence/jooq/build.gradle.kts
@@ -1,4 +1,5 @@
-import org.gradle.kotlin.dsl.execution.templateIdFor
+import org.jooq.meta.jaxb.ForcedType
+import org.jooq.meta.jaxb.Logging
 
 plugins {
     java
@@ -8,6 +9,8 @@ plugins {
     id("org.flywaydb.flyway") version "10.17.0"
     // Lombok
     id("io.freefair.lombok") version "8.6"
+    // Gradle Jooq plugin (https://github.com/etiennestuder/gradle-jooq-plugin)
+    id("nu.studer.jooq") version "9.0"
 }
 
 group = "org.demo"
@@ -35,9 +38,73 @@ dependencies {
     testImplementation("org.testcontainers:postrgresql")
     testImplementation("org.testcontainers:junit-jupiter")
     // Flyway
+    implementation("org.flywaydb:flyway-core")
     implementation("org.flywaydb:flyway-database-postgresql")
+    // Jooq starter
+    implementation("org.springframework.boot:spring-boot-starter-jooq")
+    // Jooq database driver
+    jooqGenerator("org.postgresql:postgresql:42.7.4")
 }
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+// Configure jOOQ generation tool
+jooq {
+//    version.set("3.19.1")  // default (can be omitted)
+    version.set(dependencyManagement.importedProperties["jooq.version"])
+    edition.set(nu.studer.gradle.jooq.JooqEdition.OSS)  // default (can be omitted)
+
+    configurations {
+        create("main") {  // name of the jOOQ configuration
+            generateSchemaSourceOnCompilation.set(true)  // default (can be omitted)
+
+            jooqConfiguration.apply {
+                logging = Logging.WARN
+                jdbc.apply {
+                    driver = "org.postgresql.Driver"
+                    url = "jdbc:postgresql://localhost:5432/football"
+                    user = "user"
+                    password = "user"
+//                    properties.add(Property().apply {
+//                        key = "ssl"
+//                        value = "true"
+//                    })
+                }
+                generator.apply {
+                    // // The generator class that JOOQ uses for code generation; typically DefaultGenerator is used
+                    name = "org.jooq.codegen.DefaultGenerator"
+                    database.apply {
+                        name = "org.jooq.meta.postgres.PostgresDatabase"
+                        // // The schema from which JOOQ will generate code; "public" is common for PostgreSQL
+                        inputSchema = "public"
+                        forcedTypes.addAll(listOf(
+                            ForcedType().apply {
+                                name = "varchar"
+                                includeExpression = ".*"
+                                includeTypes = "JSONB?"
+                            },
+                            ForcedType().apply {
+                                name = "varchar"
+                                includeExpression = ".*"
+                                includeTypes = "INET"
+                            }
+                        ))
+                    }
+                    generate.apply {
+                        isDeprecated = false
+                        isRecords = true
+                        isImmutablePojos = true
+                        isFluentSetters = true
+                    }
+                    target.apply {
+                        packageName = "nu.studer.sample"
+                        directory = "build/generated-src/jooq/main"  // default (can be omitted)
+                    }
+                    strategy.name = "org.jooq.codegen.DefaultGeneratorStrategy"
+                }
+            }
+        }
+    }
 }
