ad86d52d176316a96377ceb15739ae09a5c3ddf8
김용건 <yonggeon.kim@gmail.com>
2024-09-06 17:24:52 +0900

    [ReactEssentials] (router) dynamic routing

--- react-essentials/compo/src/router/RouterApp.js
+++ react-essentials/compo/src/router/RouterApp.js
@@ -1,8 +1,37 @@
 import React from "react";
-import {BrowserRouter, Link, Route, Routes} from 'react-router-dom';
+import {BrowserRouter, Link, Route, Routes, useParams} from 'react-router-dom';
 import {About} from "./About";
 import {Home} from "./Home";
 import {Contact} from "./Contact";
+
+const UserList = () => {
+    const userList = [
+        {id: 1, name: 'User 1'},
+        {id: 2, name: 'User 2'}
+    ];
+    return (
+        <div>
+            <h2>User List</h2>
+            <ul>
+                {userList.map(user => (
+                    <li key={user.id}>
+                        <Link to={`/users/${user.id}`}>{user.name}</Link>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+const UserProfile = () => {
+    const {userId} = useParams();
+    return (
+        <div>
+            <h2>User Profile</h2>
+            <p>UserId: {userId}</p>
+        </div>
+    );
+}
 
 export const RouterApp = () => {
     return (
@@ -14,12 +43,21 @@ export const RouterApp = () => {
                         <li><Link to="/about">About</Link></li>
                         <li><Link to="/contact">Contact</Link></li>
                     </ul>
+                    <ul>
+                        <li><Link to="/users">User List</Link></li>
+                        <ul>
+                            <li><Link to="/users/1">User 1</Link></li>
+                            <li><Link to="/users/2">User 2</Link></li>
+                        </ul>
+                    </ul>
                 </nav>
 
                 <Routes>
                     <Route path="/" element={<Home/>}/>
                     <Route path="/about" element={<About/>}/>
                     <Route path="/contact" element={<Contact/>}/>
+                    <Route path="/users" element={<UserList/>}/>
+                    <Route path="/users/:userId" element={<UserProfile/>}/>
                 </Routes>
             </div>
         </BrowserRouter>
