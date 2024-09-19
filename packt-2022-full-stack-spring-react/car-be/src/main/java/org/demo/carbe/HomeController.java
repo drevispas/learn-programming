1334a88976347a537cb06387999c56ad5b0db449
김용건 <yonggeon.kim@gmail.com>
2024-09-19 16:43:43 +0900

    [FSD/SR] (spring-security) default login with user and password

---
+++ packt-2022-full-stack-spring-react/car-be/src/main/java/org/demo/carbe/HomeController.java
@@ -0,0 +1,15 @@
+package org.demo.carbe;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/home")
+@RestController
+public class HomeController {
+
+    @GetMapping
+    public String home() {
+        return "home";
+    }
+}
