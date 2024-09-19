ab111566c6b46327aa5402d462439ea703671bb5
김용건 <yonggeon.kim@gmail.com>
2024-09-19 16:10:42 +0900

    [FSD/SR] (spring-security) initial project

---
+++ packt-2022-full-stack-spring-react/car-be/src/main/java/org/demo/carbe/CarBeApplication.java
@@ -0,0 +1,13 @@
+package org.demo.carbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarBeApplication.class, args);
+    }
+
+}
