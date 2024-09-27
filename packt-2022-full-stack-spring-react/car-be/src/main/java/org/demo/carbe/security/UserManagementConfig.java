f5796d1b7cd391a0f3133d690904713a457370df
김용건 <yonggeon.kim@gmail.com>
2024-09-27 14:46:42 +0900

    [FSD/SR] (security) UserDetailsService using jdbc

--- packt-2022-full-stack-spring-react/car-be/src/main/java/org/demo/carbe/security/UserManagementConfig.java
+++ packt-2022-full-stack-spring-react/car-be/src/main/java/org/demo/carbe/security/UserManagementConfig.java
@@ -2,27 +2,40 @@ package org.demo.carbe.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
-import static org.springframework.security.config.Customizer.withDefaults;
+import javax.sql.DataSource;
 
 @Configuration
 public class UserManagementConfig {
 
+    final private UserDetails userDetails = User.withUsername("user")
+            .password("user")
+            .authorities("read", "write")
+            .build();
+
     // Set a static password instead of the default random password
+//    @Bean
+    public UserDetailsService inMemoryUserDetailsService() {
+        return new InMemoryUserDetailsManager(userDetails);
+    }
+
     @Bean
-    public UserDetailsService userDetailsService() {
-        var user = User.withUsername("user")
-                .password("user")
-                .authorities("read")
-                .build();
-        return new InMemoryUserDetailsManager(user);
+    public UserDetailsService jdbcUserDetailsService(DataSource dataSource) {
+        String usersByUsernameQuery = "select username, password, enabled from spring.users where username = ?";
+        String authoritiesByUsernameQuery = "select username, authority from spring.authorities where username = ?";
+
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+//        jdbcUserDetailsManager.createUser(userDetails);
+        jdbcUserDetailsManager.setUsersByUsernameQuery(usersByUsernameQuery);
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery(authoritiesByUsernameQuery);
+        return jdbcUserDetailsManager;
     }
 
     @Bean
---
+++ packt-2022-full-stack-spring-react/car-be/src/main/resources/schema.sql
@@ -0,0 +1,15 @@
+CREATE TABLE if not exists spring.users
+(
+    username VARCHAR(50)  NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    enabled  TINYINT      NOT NULL DEFAULT 1,
+    PRIMARY KEY (username)
+);
+
+CREATE TABLE if not exists spring.authorities
+(
+    username  VARCHAR(50) NOT NULL,
+    authority VARCHAR(50) NOT NULL,
+    FOREIGN KEY (username) REFERENCES users (username),
+    UNIQUE (username, authority)
+);
