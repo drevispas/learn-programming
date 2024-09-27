c41c9ff174b8b8266fb2262f856190cc5ee71c81
김용건 <yonggeon.kim@gmail.com>
2024-09-27 10:30:24 +0900

    [FSD/SR] (security) basic custom spring security

--- packt-2022-full-stack-spring-react/car-be/src/main/java/org/demo/carbe/security/UserManagementConfig.java
+++ packt-2022-full-stack-spring-react/car-be/src/main/java/org/demo/carbe/security/UserManagementConfig.java
@@ -3,37 +3,30 @@ package org.demo.carbe.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
-public class SecurityConfig {
+public class UserManagementConfig {
 
     // Set a static password instead of the default random password
     @Bean
     public UserDetailsService userDetailsService() {
         var user = User.withUsername("user")
-                .password("{noop}user")
+                .password("user")
                 .authorities("read")
                 .build();
         return new InMemoryUserDetailsManager(user);
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http.authorizeHttpRequests(authorizeRequests ->
-                authorizeRequests
-                        .requestMatchers("/**").permitAll()
-                        .anyRequest().authenticated()
-        )
-//                .formLogin(withDefaults())
-//                .csrf(csrf -> csrf.ignoringRequestMatchers("/h2/**"))
-                .csrf(AbstractHttpConfigurer::disable)
-                .build();
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
