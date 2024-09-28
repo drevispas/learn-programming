b11e54c240b4679e85078271651ea66c65166e9b
김용건 <yonggeon.kim@gmail.com>
2024-09-28 17:27:23 +0900

    [FSD/SR] (car-fe) delete button

--- packt-2022-full-stack-spring-react/car-fe/src/components/Carlist.js
+++ packt-2022-full-stack-spring-react/car-fe/src/components/Carlist.js
@@ -5,11 +5,30 @@ export function Carlist() {
 
     const [cars, setCars] = useState([]);
 
-    useEffect(() => {
+    const fetchCars = () => {
         fetch('http://localhost:8080/api/cars')
             .then(response => response.json())
             .then(data => setCars(data._embedded.cars))
             .catch(err => console.error(err))
+    }
+
+    const deleteCar = (url) => {
+        console.log('Deleting car with url: ' + url);
+        fetch(url, {
+            method: 'DELETE'
+        })
+            .then(response => {
+                if (response.ok) {
+                    fetchCars();
+                } else {
+                    alert('Failed to delete car');
+                }
+            })
+            .catch(err => console.error(err));
+    }
+
+    useEffect(() => {
+        fetchCars();
     }, []);
 
     const columns = [
@@ -19,12 +38,24 @@ export function Carlist() {
         {field: 'manufacturingYear', headerName: 'Manufacturing Year', width: 150},
         {field: 'price', headerName: 'Price', width: 150},
         {field: 'registerNumber', headerName: 'Register Number', width: 150},
+        {
+            field: '_links.self.href',
+            headerName: 'Actions',
+            width: 150,
+            renderCell: (row) => (
+                <div>
+                    <button onClick={() => deleteCar(row.id)}>Delete</button>
+                </div>
+            )
+        }
     ]
 
     return (
         <div style={{height:500, width:'100%'}}>
             <DataGrid columns={columns} rows={cars}
-                      getRowId={row => row._links.self.href}/>
+                      getRowId={row => row._links.self.href}
+                      disableRowSelectionOnClick={true}
+            />
         </div>
     )
 }
