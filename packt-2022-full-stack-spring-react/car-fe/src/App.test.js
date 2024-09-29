42ce4200b27cb0f21930709212956387814031ea
김용건 <yonggeon.kim@gmail.com>
2024-09-29 13:19:57 +0900

    [FSD/SR] (car-fe) component test snapshot

--- packt-2022-full-stack-spring-react/car-fe/src/App.test.js
+++ packt-2022-full-stack-spring-react/car-fe/src/App.test.js
@@ -1,5 +1,12 @@
 import {fireEvent, render, screen} from '@testing-library/react';
 import App from './App';
+import TestRednerer from 'react-test-renderer';
+import {AddCar} from "./components/AddCar";
+
+test('renders a snapshot', () => {
+  const tree = TestRednerer.create(<AddCar />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
 
 test('open Add car modal form', () => {
   render(<App />);
