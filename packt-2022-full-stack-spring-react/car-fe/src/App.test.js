b0ebb2e4c6c63a69da3fd4f841d1b82ed9b12f34
김용건 <yonggeon.kim@gmail.com>
2024-09-29 13:13:44 +0900

    [FSD/SR] (car-fe) simple unit test using Jest

--- packt-2022-full-stack-spring-react/car-fe/src/App.test.js
+++ packt-2022-full-stack-spring-react/car-fe/src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
+import {fireEvent, render, screen} from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('open Add car modal form', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  fireEvent.click(screen.getByText('New Car'));
+  expect(screen.getByRole('dialog')).toHaveTextContent('Add car');
 });
