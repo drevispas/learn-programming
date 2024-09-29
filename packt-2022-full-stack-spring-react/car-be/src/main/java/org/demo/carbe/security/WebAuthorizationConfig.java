81de952c5dd23cb651f75b5a98d5f222f9a08109
김용건 <yonggeon.kim@gmail.com>
2024-09-29 14:14:44 +0900

    [FSD/Spring] (security) turn on security

--- packt-2022-full-stack-spring-react/car-be/src/main/java/org/demo/carbe/security/WebAuthorizationConfig.java
+++ packt-2022-full-stack-spring-react/car-be/src/main/java/org/demo/carbe/security/WebAuthorizationConfig.java
@@ -6,18 +6,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-
-import java.util.List;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @RequiredArgsConstructor
 @Configuration
@@ -27,13 +20,13 @@ public class WebAuthorizationConfig {
     private final JwtFilter jwtFilter;
     private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
 
-    //    @Bean
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http.cors(corsCustomizer);
         http.cors(corsCustomizer);
         http.csrf(AbstractHttpConfigurer::disable);
         // the authorization rules at the endpoints
-        http.authorizeHttpRequests(authorizeRequests ->
+        http
+                .authorizeHttpRequests(authorizeRequests ->
                         authorizeRequests
                                 .requestMatchers("/token/**").permitAll()
                                 .requestMatchers("/swagger-ui/**").permitAll()
@@ -46,7 +39,8 @@ public class WebAuthorizationConfig {
         return http.build();
     }
 
-    @Bean
+    // Switch to this bean to allow all requests to pass through
+//    @Bean
     public SecurityFilterChain openedSecurityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .cors(corsCustomizer)
