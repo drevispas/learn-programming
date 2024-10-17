6fb44c70a74bb968b62ef9cbaae7d31334d2975d
김용건 <yonggeon.kim@gmail.com>
2024-10-17 13:11:45 +0900

    savepoint

---
+++ roadbook-2024-react-shopping-mall/react-by-cdn/App.js
@@ -0,0 +1,9 @@
+const App = () => {
+    return (
+        <>
+            <h1>Hello World!</h1>
+        </>
+    )
+}
+const root = document.querySelector("#root");
+ReactDOM.render(<App />, root);
